5e3c42ade606242f7e2bd6b3bab7a83c303992a5
Martin <martinwjwilson@gmail.com>
2022-12-02 17:38:03 +0000

    Choices into classes
    Turned each choice into a class that knows what it beats and loses against

--- day-02/part1.py
+++ day-02/part1.py
@@ -1,60 +1,56 @@
-# get the input file contents into a variable
-f = open('input.txt', 'r')
-content = f.read()
-list_of_rounds = content.split("\n")
+class Choice:
+    def __init__(self, wins_against, loses_against, value):
+        self.wins_against = wins_against
+        self.loses_against = loses_against
+        self.value = value
+
+
+class Rock(Choice):
+    def __init__(self):
+        super().__init__(Scissors, Paper, 1)
+
+
+class Paper(Choice):
+    def __init__(self):
+        super().__init__(Rock, Scissors, 2)
+
+
+class Scissors(Choice):
+    def __init__(self):
+        super().__init__(Paper, Rock, 3)
+
+
+def calculate_round_winner(your_choice: Choice, opponent_choice: Choice):
+    if type(your_choice) == type(opponent_choice):
+        return outcome_values["draw"]
+    elif your_choice.wins_against == type(opponent_choice):
+        return outcome_values["win"]
+    elif your_choice.loses_against == type(opponent_choice):
+        return outcome_values["loss"]
+
+
+def convert_moves(choice: str) -> Choice:
+    if choice in ("A", "X"):
+        return Rock()
+    elif choice in ("B", "Y"):
+        return Paper()
+    return Scissors()
+
 
 # Constants
 outcome_values = {"loss": 0,
                   "draw": 3,
                   "win": 6}
-possible_choices = {"R": 1,
-                    "P": 2,
-                    "S": 3}
 
+# get the input file contents into a variable
+f = open('input.txt', 'r')
+content = f.read()
+list_of_rounds = content.split("\n")
 
-def calculate_round_winner(your_choice, opponent_choice) -> int:
-    # return "you", "opponent" or "draw" depending on who won
-    if your_choice == opponent_choice:
-        return draw_value
-
-    if your_choice == "R":
-        if opponent_choice == "P":
-            return outcome_values["loss"]
-        if opponent_choice == "S":
-            return outcome_values["win"]
-    if your_choice == "P":
-        if opponent_choice == "S":
-            return outcome_values["loss"]
-        if opponent_choice == "R":
-            return outcome_values["win"]
-    if your_choice == "S":
-        if opponent_choice == "R":
-            return outcome_values["loss"]
-        if opponent_choice == "P":
-            return outcome_values["win"]
-
-
-def calculate_choice_score(your_choice: str) -> int:
-    return possible_choices[your_choice]
-
-
-def convert_moves(choice: str) -> str:
-    if choice in ("A", "X"):
-        return "R"
-    elif choice in ("B", "Y"):
-        return "P"
-    return "S"
-
-
-# convert choices into strings
 total_score = 0
 for current_round in list_of_rounds:
     your_choice = convert_moves(current_round[2])
     opponent_choice = convert_moves(current_round[0])
-    # round winner
-    round_outcome = calculate_round_winner(your_choice, opponent_choice)
-    your_choice_score = calculate_choice_score(your_choice)
-
-    total_score += (round_outcome + your_choice_score)
+    total_score += (calculate_round_winner(your_choice, opponent_choice) + your_choice.value)
 
 print(total_score)
