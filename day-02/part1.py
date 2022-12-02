8c08adcf162bc4b04ff964457cb1c4e330bd11bf
Martin <martinwjwilson@gmail.com>
2022-12-02 17:02:49 +0000

    Improvements
    Making changes and improvements to clean up the code after getting the right answer

--- day-02/part1.py
+++ day-02/part1.py
@@ -3,15 +3,14 @@ f = open('input.txt', 'r')
 content = f.read()
 list_of_rounds = content.split("\n")
 
-# round outcome values
-loss_value = 0
-draw_value = 3
-win_value = 6
-
-# list of choices in order
+# Constants
+outcome_values = {"loss": 0,
+                  "draw": 3,
+                  "win": 6}
 possible_choices = {"R": 1,
                     "P": 2,
                     "S": 3}
+
 
 def calculate_round_winner(your_choice, opponent_choice) -> int:
     # return "you", "opponent" or "draw" depending on who won
@@ -20,19 +19,19 @@ def calculate_round_winner(your_choice, opponent_choice) -> int:
 
     if your_choice == "R":
         if opponent_choice == "P":
-            return loss_value
+            return outcome_values["loss"]
         if opponent_choice == "S":
-            return win_value
+            return outcome_values["win"]
     if your_choice == "P":
         if opponent_choice == "S":
-            return loss_value
+            return outcome_values["loss"]
         if opponent_choice == "R":
-            return win_value
+            return outcome_values["win"]
     if your_choice == "S":
         if opponent_choice == "R":
-            return loss_value
+            return outcome_values["loss"]
         if opponent_choice == "P":
-            return win_value
+            return outcome_values["win"]
 
 
 def calculate_choice_score(your_choice: str) -> int:
