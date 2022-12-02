a5379b62e38c997b0684ea77422477be96518e0e
Martin <martinwjwilson@gmail.com>
2022-12-02 23:00:36 +0000

    Neatening up with classes
    Used the implementation for part 1 and extended it to fit the second part

--- day-02/part2.py
+++ day-02/part2.py
@@ -1,61 +1,49 @@
-# get the input file contents into a variable
-f = open('input.txt', 'r')
-content = f.read()
-list_of_rounds = content.split("\n")
-
-# round outcome values
-loss_value = 0
-draw_value = 3
-win_value = 6
-
-# list of choices in order
-possible_choices = {"R": 1,
-                    "P": 2,
-                    "S": 3}
-
-def calculate_round_winner(your_choice, opponent_choice) -> int:
-    # return "you", "opponent" or "draw" depending on who won
-    if your_choice == opponent_choice:
-        return draw_value
-
-    if your_choice == "R":
-        if opponent_choice == "P":
-            return loss_value
-        if opponent_choice == "S":
-            return win_value
-    if your_choice == "P":
-        if opponent_choice == "S":
-            return loss_value
-        if opponent_choice == "R":
-            return win_value
-    if your_choice == "S":
-        if opponent_choice == "R":
-            return loss_value
-        if opponent_choice == "P":
-            return win_value
+class Choice:
+    def __init__(self, wins_against, loses_against, value):
+        self.wins_against = wins_against
+        self.loses_against = loses_against
+        self.value = value
 
 
-def calculate_choice_score(your_choice: str) -> int:
-    return possible_choices[your_choice]
+class Rock(Choice):
+    def __init__(self):
+        super().__init__(Scissors, Paper, 1)
 
 
-def calculate_your_choice(opponent_choice, outcome):
+class Paper(Choice):
+    def __init__(self):
+        super().__init__(Rock, Scissors, 2)
+
+
+class Scissors(Choice):
+    def __init__(self):
+        super().__init__(Paper, Rock, 3)
+
+
+def calculate_round_winner(your_choice: Choice, opponent_choice: Choice):
+    if type(your_choice) == type(opponent_choice):
+        return outcome_values["draw"]
+    elif your_choice.wins_against == type(opponent_choice):
+        return outcome_values["win"]
+    elif your_choice.loses_against == type(opponent_choice):
+        return outcome_values["loss"]
+
+
+def calculate_your_choice(opponent_choice: Choice, outcome: str) -> Choice:
     if outcome == "Lose":
-        if opponent_choice == "R":
-            return "S"
-        if opponent_choice == "P":
-            return "R"
-        if opponent_choice == "S":
-            return "P"
+        return opponent_choice.wins_against()
     if outcome == "Draw":
         return opponent_choice
     if outcome == "Win":
-        if opponent_choice == "R":
-            return "P"
-        if opponent_choice == "P":
-            return "S"
-        if opponent_choice == "S":
-            return "R"
+        return opponent_choice.loses_against()
+
+
+def convert_moves(choice: str) -> Choice:
+    if choice in ("A", "X"):
+        return Rock()
+    elif choice in ("B", "Y"):
+        return Paper()
+    return Scissors()
 
 
 def convert_to_outcome(letter: str) -> str:
@@ -66,27 +54,21 @@ def convert_to_outcome(letter: str) -> str:
     return "Win"
 
 
-def convert_moves(choice: str) -> str:
-    if choice in ("A", "X"):
-        return "R"
-    elif choice in ("B", "Y"):
-        return "P"
-    return "S"
+# Constants
+outcome_values = {"loss": 0,
+                  "draw": 3,
+                  "win": 6}
 
+# get the input file contents into a variable
+f = open('input.txt', 'r')
+content = f.read()
+list_of_rounds = content.split("\n")
 
-# convert choices into strings
 total_score = 0
 for current_round in list_of_rounds:
     expected_outcome = convert_to_outcome(current_round[2])
     opponent_choice = convert_moves(current_round[0])
-    # round winner
     your_choice = calculate_your_choice(opponent_choice, expected_outcome)
-    round_outcome = calculate_round_winner(your_choice, opponent_choice)
-    your_choice_score = calculate_choice_score(your_choice)
-
-    print(f"Your choice {your_choice} and choice score {your_choice_score}")
-    print(f"Round outcome was {round_outcome}")
-    total_score += (round_outcome + your_choice_score)
-    print(f"Total score {total_score}\n")
+    total_score += (calculate_round_winner(your_choice, opponent_choice) + your_choice.value)
 
 print(total_score)
