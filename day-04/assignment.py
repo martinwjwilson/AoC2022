a7b4f5dfb1b13596492a14cb493a01bb1f94f10c
Martin <martinwjwilson@gmail.com>
2022-12-04 17:26:46 +0000

    Part 1 solution

---
+++ day-04/assignment.py
@@ -0,0 +1,8 @@
+class Assignment:
+    def __init__(self, assignment_range: [int, int]):
+        self.starting_section = assignment_range[0]
+        self.ending_section = assignment_range[1]
+        self.section_range = list(range(self.starting_section, self.ending_section + 1))
+
+    def fully_contains(self, alternative_assignment_range) -> bool:
+        return set(self.section_range).issuperset(set(alternative_assignment_range))
