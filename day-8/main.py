2428e634c608eaea775733e3bd2b6191a88e6e7d
Martin <martinwjwilson@gmail.com>
2022-12-09 02:17:30 +0000

    Part 2 solution

--- day-8/main.py
+++ day-8/main.py
@@ -37,7 +37,7 @@ def calculate_border_count(last_tree):
     return (last_tree.x + last_tree.y) * 2
 
 
-def calculate_number_of_visible_trees(forest):
+def calculate_number_of_visible_trees(forest) -> int:
     total_number_of_visible_trees = 0
     # calculate number of trees on border as they're all visible
     total_number_of_visible_trees += calculate_border_count(forest[-1])
@@ -48,7 +48,56 @@ def calculate_number_of_visible_trees(forest):
         if (tree.x != 0) and (tree.y != 0) and (tree.x != maximum_x) and (tree.y != maximum_y):
             if is_tree_visible(forest, tree):
                 tree_counter += 1
-    print(f"There are {total_number_of_visible_trees + tree_counter} visible trees")
+    return total_number_of_visible_trees + tree_counter
+
+
+def calculate_scenic_scores(current_tree, forest) -> [int]:
+    north_score = 0
+    east_score = 0
+    west_score = 0
+    south_score = 0
+    maximum_x = forest[-1].x
+    maximum_y = forest[-1].y
+    for comparison_tree in forest:
+        # North
+        if (comparison_tree.y < current_tree.y) and (comparison_tree.x == current_tree.x):
+            if comparison_tree.y == 0:
+                north_score = current_tree.y
+            elif comparison_tree.height >= current_tree.height:
+                north_score = current_tree.y - comparison_tree.y
+        # West
+        if (comparison_tree.y == current_tree.y) and (comparison_tree.x < current_tree.x):
+            if comparison_tree.x == 0:
+                west_score = current_tree.x
+            elif comparison_tree.height >= current_tree.height:
+                west_score = current_tree.x - comparison_tree.x
+        # East
+        if (comparison_tree.y == current_tree.y) and (comparison_tree.x > current_tree.x):
+            if east_score == 0:
+                if comparison_tree.height >= current_tree.height:
+                    east_score = comparison_tree.x - current_tree.x
+                elif comparison_tree.x == maximum_x:
+                    east_score = maximum_x - current_tree.x
+        # South
+        if (comparison_tree.y > current_tree.y) and (comparison_tree.x == current_tree.x):
+            if south_score == 0:
+                if comparison_tree.height >= current_tree.height:
+                    south_score = comparison_tree.y - current_tree.y
+                elif comparison_tree.y == maximum_y:
+                    south_score = maximum_y - current_tree.y
+    print(f"{north_score},{west_score},{south_score},{east_score}")
+    return [north_score, west_score, south_score, east_score]
+
+
+def calculate_highest_scenic_score(forest) -> int:
+    highest_score = 0
+    for tree in forest:
+        scenic_scores = calculate_scenic_scores(tree, forest)
+        tree.set_scenic_score(scenic_scores[0], scenic_scores[1], scenic_scores[2], scenic_scores[3])
+        if tree.scenic_score > highest_score:
+            highest_score = tree.scenic_score
+            print(f"The tree is {tree.x},{tree.y} with a score of {tree.scenic_score}")
+    return highest_score
 
 
 if __name__ == '__main__':
@@ -66,7 +115,5 @@ if __name__ == '__main__':
             x_coordinate += 1
         y_coordinate += 1
 
-    test_tree = forest[9]
-    print(f"The height of the tree at ({test_tree.x},{test_tree.y}) = {test_tree.height}")
-
-    calculate_number_of_visible_trees(forest)
+    # print(f"The solution for part 1 is: {calculate_number_of_visible_trees(forest)}")
+    print(f"The solution for part 2 is: {calculate_highest_scenic_score(forest)}")
--- day-8/tree.py
+++ day-8/tree.py
@@ -3,3 +3,7 @@ class Tree:
         self.height = height
         self.x = x_coordinate
         self.y = y_coordinate
+        self.scenic_score = 0
+
+    def set_scenic_score(self, n, s, e, w):
+        self.scenic_score = n * s * e * w
